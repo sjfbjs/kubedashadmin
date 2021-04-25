3f30b71553a73c28e33913ac7d801a423f64d586
shisuan <shisuan@myweimai.com>
2021-04-25 14:50:41 +0800

    添加前端pod相关页面路由

--- vue-admin/src/router/index.js
+++ vue-admin/src/router/index.js
@@ -97,75 +97,29 @@ export const constantRoutes = [
   },
 
   {
-    path: '/nested',
+    path: '/pod',
     component: Layout,
-    redirect: '/nested/menu1',
-    name: 'Nested',
+    redirect: '/pod/podlist',
+    name: 'Pod',
     meta: {
-      title: 'Nested',
-      icon: 'nested'
+      title: 'Pod',
+      icon: 'pod'
     },
     children: [
       {
-        path: 'menu1',
-        component: () => import('@/views/nested/menu1/index'), // Parent router-view
-        name: 'Menu1',
-        meta: { title: 'Menu1' },
-        children: [
-          {
-            path: 'menu1-1',
-            component: () => import('@/views/nested/menu1/menu1-1'),
-            name: 'Menu1-1',
-            meta: { title: 'Menu1-1' }
-          },
-          {
-            path: 'menu1-2',
-            component: () => import('@/views/nested/menu1/menu1-2'),
-            name: 'Menu1-2',
-            meta: { title: 'Menu1-2' },
-            children: [
-              {
-                path: 'menu1-2-1',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
-                name: 'Menu1-2-1',
-                meta: { title: 'Menu1-2-1' }
-              },
-              {
-                path: 'menu1-2-2',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
-                name: 'Menu1-2-2',
-                meta: { title: 'Menu1-2-2' }
-              }
-            ]
-          },
-          {
-            path: 'menu1-3',
-            component: () => import('@/views/nested/menu1/menu1-3'),
-            name: 'Menu1-3',
-            meta: { title: 'Menu1-3' }
-          }
-        ]
+        path: 'podlist',
+        component: () => import('@/views/pod/podlist/index'), // Parent router-view
+        name: 'Podlist',
+        meta: { title: 'Podlist' }
       },
       {
-        path: 'menu2',
-        component: () => import('@/views/nested/menu2/index'),
-        meta: { title: 'menu2' }
+        path: 'logs',
+        component: () => import('@/views/pod/logs/index'),
+        meta: { title: 'Logs' }
       }
     ]
   },
-
-  {
-    path: 'external-link',
-    component: Layout,
-    children: [
-      {
-        path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
-        meta: { title: 'External Link', icon: 'link' }
-      }
-    ]
-  },
-
-  // 404 page must be placed at the end !!!
+  // 404 page must be placed at the end !!
   { path: '*', redirect: '/404', hidden: true }
 ]
 
